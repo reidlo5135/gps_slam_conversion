0be9ae3938b6fba32ef2a95f67ac7e0734630475
reidlo5135 <201840103@email.daelim.ac.kr>
2023-11-04 12:03:43 +0900

    feat : add map_coordinates.yaml parameter

--- gps_slam_converter/include/gps_slam_converter/utils.hxx
+++ gps_slam_converter/include/gps_slam_converter/utils.hxx
@@ -3,23 +3,33 @@
 
 #include <rcutils/logging_macros.h>
 
-static constexpr const double &INTERSECTION_START_POINT_LON = 128.858009083;
-static constexpr const double &INTERSECTION_START_POINT_LAT = 35.157430158;
-static constexpr const double &INTERSECTION_END_POINT_LON = 128.858870603;
-static constexpr const double &INTERSECTION_END_POINT_LAT = 35.158056682;
+static constexpr const double &INTERSECTION_START_POINT_LON_DEFAULT = 128.858009083;
+static constexpr const double &INTERSECTION_START_POINT_LAT_DEFAULT = 35.157430158;
+static constexpr const double &INTERSECTION_END_POINT_LON_DEFAULT = 128.858870603;
+static constexpr const double &INTERSECTION_END_POINT_LAT_DEFAULT = 35.158056682;
 
 static constexpr const int &SLAM_MAP_WIDTH = 2080;
 static constexpr const int &SLAM_MAP_HEIGHT = 1200;
 
-static constexpr const double &LB_POINT_LON = 128.8575686;
-static constexpr const double &LB_POINT_LAT = 35.157430158;
-static constexpr const double &RT_POINT_LON = 128.858870603;
-static constexpr const double &RT_POINT_LAT = 35.158463143;
+static constexpr const double &LB_POINT_LON_DEFAULT = 128.8575686001505289;
+static constexpr const double &LB_POINT_LAT_DEFAULT = 35.1574301543503580;
+static constexpr const double &RT_POINT_LON_DEFAULT = 128.8588706034498728;
+static constexpr const double &RT_POINT_LAT_DEFAULT = 35.1584631433985280;
 
 static constexpr const char *RCL_NODE_NAME = "gps_slam_converter";
 static constexpr const int &RCL_STOP_FLAG = 0;
 static constexpr const int &RCL_DEFAULT_INT = 0;
 static constexpr const double &RCL_DEFAULT_DOUBLE = 0.0;
+static constexpr const char *RCL_SLAM_MAP_WIDTH_PARAMETER = "slam_map_width";
+static constexpr const char *RCL_SLAM_MAP_HEIGHT_PARAMETER = "slam_map_height";
+static constexpr const char *RCL_INTERSECTION_START_POINT_LON_PARAMTER = "intersection_start_point_lon";
+static constexpr const char *RCL_INTERSECTION_START_POINT_LAT_PARAMTER = "intersection_start_point_lat";
+static constexpr const char *RCL_INTERSECTION_END_POINT_LON_PARAMTER = "intersection_end_point_lon";
+static constexpr const char *RCL_INTERSECTION_END_POINT_LAT_PARAMTER = "intersection_end_point_lat";
+static constexpr const char *RCL_LB_POINT_LON_PARAMETER = "lb_point_lon";
+static constexpr const char *RCL_LB_POINT_LAT_PARAMETER = "lb_point_lat";
+static constexpr const char *RCL_RT_POINT_LON_PARAMETER = "rt_point_lon";
+static constexpr const char *RCL_RT_POINT_LAT_PARAMETER = "rt_point_lat";
 static constexpr const char *RCL_ROBOT_POSE_TOPIC = "/robot_pose";
 static constexpr const char *RCL_UBLOX_FIX_TOPIC = "/ublox/fix";
 static constexpr const char *RCL_GPS_TO_SLAM_TOPIC = "/gps_to_slam";
--- gps_slam_converter/launch/converter.launch.py
+++ gps_slam_converter/launch/converter.launch.py
@@ -3,19 +3,24 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
-def generate_launch_description():
-    ld = LaunchDescription()
+def generate_launch_description() -> LaunchDescription:
+    ld: LaunchDescription = LaunchDescription()
 
     package_name: str = 'gps_slam_converter'
     package_shared_directory: str = get_package_share_directory(package_name)
-    executable_name: str = 'converter'
+
+    map_coordinates_parameter: str = os.path.join(
+        package_shared_directory,
+        'config',
+        'map_coordinates.yaml'
+    )
     
     converter_node = Node(
         package=package_name,
-        executable=executable_name,
-        name=executable_name,
+        executable=package_name,
+        name=package_name,
         output='screen',
-        parameters=[]
+        parameters=[map_coordinates_parameter]
     )
     
     ld.add_action(converter_node)
